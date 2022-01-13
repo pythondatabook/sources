e6960d22547368eaaafa5865bba766aef1cb4a44
pythondatabook <97634112+pythondatabook@users.noreply.github.com>
2022-01-13 07:40:03 -0500

    Create urllib3_example2.py

---
+++ ch4/urllib3_example2.py
@@ -0,0 +1,10 @@
+import json
+import urllib3
+http = urllib3.PoolManager()
+r = http.request('GET', 'https://newsapi.org/v2/everything?q=Python programming language&apiKey=your_newsapi_key&pageSize=5')
+articles = json.loads(r.data.decode('utf-8'))
+for article in articles['articles']:
+  print(article['title'])
+  print(article['publishedAt'])  
+  print(article['url'])
+  print() 
