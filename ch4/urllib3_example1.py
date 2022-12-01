aa4d6704405a8e34ab368693a5a5af8b39d84c30
pythondatabook <97634112+pythondatabook@users.noreply.github.com>
2022-12-01 10:16:41 -0500

    Create urllib3_example1.py

---
+++ ch4/urllib3_example1.py
@@ -0,0 +1,6 @@
+import urllib3
+http = urllib3.PoolManager()
+r = http.request('GET', 'http://localhost/excerpt.txt')
+for i, line in enumerate(3 r.data.decode('utf-8').split('\n')):
+  if line.strip():
+    print("Line %i: " %(i), line.strip())
