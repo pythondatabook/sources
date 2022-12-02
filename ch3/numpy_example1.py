d75a74aa04b3470e0e5bc300bb37912d1dd1b87d
pythondatabook <97634112+pythondatabook@users.noreply.github.com>
2022-12-02 10:16:40 -0500

    Create numpy_example1.py

---
+++ ch3/numpy_example1.py
@@ -0,0 +1,8 @@
+# pp 38
+
+import numpy as np
+jeff_salary = [2700,3000,3000]
+nick_salary = [2600,2800,2800]
+tom_salary = [2300,2500,2500]
+base_salary = np.array([jeff_salary, nick_salary, tom_salary])
+print(base_salary)
