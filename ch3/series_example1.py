d1d7acaba8afed73ec909e9e85c566632d757533
pythondatabook <97634112+pythondatabook@users.noreply.github.com>
2022-12-03 10:15:38 -0500

    Update series_example1.py

--- ch3/series_example1.py
+++ ch3/series_example1.py
@@ -4,3 +4,5 @@ import pandas as pd
 data = ['Jeff Russell','Jane Boorman','Tom Heints']
 emps_names = pd.Series(data)
 print(emps_names)
+emps_names = pd.Series(data,index=[9001,9002,9003])
+print(emps_names.loc[9001:9002])
