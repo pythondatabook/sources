f316572a84eabb5e652e16b9a80530317558e30e
pythondatabook <97634112+pythondatabook@users.noreply.github.com>
2022-12-05 10:53:20 -0500

    Create percentage_changes.py

---
+++ percentage_changes.py
@@ -0,0 +1,10 @@
+# pp 163 - 165  
+
+import yfinance as yf
+import numpy as np
+ticker = 'TSLA'
+tkr = yf.Ticker(ticker)
+df = tkr.history(period='5d')
+print(pd.concat([df['Close'], df['Close'].shift(2)], axis=1, keys= ['Close', '2DaysShift']))
+df['2daysRise'] = np.log(df['Close'] / df['Close'].shift(2))
+print(df[['Close','2daysRise']])  
