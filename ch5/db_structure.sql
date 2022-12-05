116bd761c01a654ee7daafeb6a5d7e81e0cc40d5
pythondatabook <97634112+pythondatabook@users.noreply.github.com>
2022-12-05 08:05:12 -0500

    Create db_structure.sql

---
+++ ch5/db_structure.sql
@@ -0,0 +1,21 @@
+# pp 77 - 78
+
+CREATE TABLE emps (
+ empno INT NOT NULL,
+ empname VARCHAR(50),
+ job VARCHAR(30),
+ PRIMARY KEY (empno)
+);
+CREATE TABLE salary (
+ empno INT NOT NULL,
+ salary INT,
+ PRIMARY KEY (empno)
+);
+ALTER TABLE salary ADD FOREIGN KEY (empno) REFERENCES emps (empno);
+CREATE TABLE orders (
+ pono INT NOT NULL,
+ empno INT NOT NULL,
+ total INT,
+ PRIMARY KEY (pono),
+ FOREIGN KEY (empno) REFERENCES emps (empno)
+);
