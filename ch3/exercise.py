ebd24156ad0d87f61915b10598ed8f5ef79d1443
pythondatabook <97634112+pythondatabook@users.noreply.github.com>
2022-12-11 08:38:55 -0500

    Create exercise.py

---
+++ ch3/exercise.py
@@ -0,0 +1,20 @@
+# pp 38 - 40 
+
+import numpy as np
+jeff_salary = [2700,3000,3000]
+nick_salary = [2600,2800,2800]
+tom_salary = [2300,2500,2500]
+base_salary = np.array([jeff_salary, nick_salary, tom_salary])
+
+jeff_bonus = [500,400,400]
+nick_bonus = [600,300,400]
+tom_bonus = [200,500,400]
+bonus = np.array([jeff_bonus, nick_bonus, tom_bonus])
+
+salary_bonus = base_salary + bonus
+print(type(salary_bonus))
+print(salary_bonus)
+
+print(np.amax(salary_bonus, axis = 0))
+
+print(np.amax(np.amax(salary_bonus, axis = 0)))
