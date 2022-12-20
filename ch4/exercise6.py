5f025b8278556e244c1ee0217da4fe2bb9ef5f22
pythondatabook <97634112+pythondatabook@users.noreply.github.com>
2022-12-20 06:42:28 -0500

    Create exercise6.py

---
+++ ch4/exercise6.py
@@ -0,0 +1,13 @@
+import requests
+import json
+params = {
+'q':'Python programming language',
+'apiKey': 'your API key',
+'pageSize': 5} 
+r = requests.get('https://newsapi.org/v2/everything',params)
+articles = json.loads(r.text)
+for article in articles['articles']:
+  print(article['title'])
+  print(article['publishedAt'])
+  print(article['url'])
+  print()
