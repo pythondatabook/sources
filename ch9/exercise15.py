b503548328599da58fc1744d7af3c2fa59f61fd9
pythondatabook <97634112+pythondatabook@users.noreply.github.com>
2023-01-06 10:56:36 -0500

    Update exercise15.py

--- ch9/exercise15.py
+++ ch9/exercise15.py
@@ -11,9 +11,15 @@ cabs = [cab_26,cab_112]
 cabs_names = ['cab_26', 'cab_112']
 pick_up = Point(46.080074, 38.991289)
 entry_point = Point(46.075357, 39.000298)
+closest_dist = 1000000
+closest_car = ''
 for i, cab in enumerate(cabs):
   if cab.within(poly):
     dist = distance((pick_up.x, pick_up.y), (cab.x,cab.y)).m
   else:
     dist = distance((cab.x,cab.y), (entry_point.x,entry_point.y)).m + distance((entry_point.x,entry_point.y), (pick_up.x, pick_up.y)).m
+  if dist < closest_dist:
+    closest_dist = dist
+    closest_car = cabs_names[i]
   print(cabs_names[i],':', round(dist))
+print('Closest car: ', closest_car)  
